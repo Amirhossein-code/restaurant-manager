3061bcc35bc625bf6f7c12980f56b20470183801
Amir <edadiamirhossein@gmail.com>
2024-02-02 02:51:39 -0500

    initial configurations

---
+++ Backend/API/settings/local.py
@@ -0,0 +1,18 @@
+from .common import *
+
+SECRET_KEY = "django-insecure-)hf=%kwj)utr&6!*k)#kq^o5by0=8i45#r7%u!o53)dcldx7a_"
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "mysql.connector.django",
+        "NAME": "menu_manager",
+        "USER": "root",
+        "PASSWORD": "MO@142aheRTU",
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
