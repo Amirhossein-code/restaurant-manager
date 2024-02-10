d6bbe3f50c0c8afaa4a546f329c17478c4e84240
Amir <edadiamirhossein@gmail.com>
2024-02-10 03:40:08 -0500

    add api homepage

--- Backend/API/urls.py
+++ Backend/API/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('' , include('core.urls')),
     path("admin/", admin.site.urls),
     path("app/", include("app.urls")),
     # path("control_center/", include("control_center.urls")),
