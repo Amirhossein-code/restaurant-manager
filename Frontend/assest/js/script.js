005dfc7834843731c5575fcf50450dc0ec96d348
Amir <edadiamirhossein@gmail.com>
2024-02-26 03:53:17 -0500

    update base url

--- Frontend/assest/js/script.js
+++ Frontend/assest/js/script.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:8000/'
+const baseUrl = "http://5.34.196.45:8000/";
 
 //url
 const categoriesUrl = `${baseUrl}app/categories/`;
@@ -6,7 +6,9 @@ const itemsUrl = `${baseUrl}app/items/`;
 
 //valriables we use i
 const menuListContainer = document.getElementById("menu-list-container");
-const firstMenuListContainer = document.getElementById("first-menu-list-container")
+const firstMenuListContainer = document.getElementById(
+  "first-menu-list-container"
+);
 const categoryContainer = document.getElementById("categori-container");
 const swiperWrapper = document.getElementById("swiper-wrapper");
 const preLoad = document.getElementById("preload");
@@ -14,19 +16,23 @@ const preLoad = document.getElementById("preload");
 window.onload = entery();
 
 function entery() {
-  preLoad.style.display = 'none';
-  fetch(categoriesUrl).then(res => res.json()).then((data) => {
-    categoryCreate(data.results)
-  });
-
-  fetch(itemsUrl).then((res) => res.json()).then((data) => {
-    itemCreate(data.results);
-    $grid = $('.menu-list-container').isotope({
-      itemSelector: '.menu-item',
-      layoutMode: 'fitRows'
+  preLoad.style.display = "none";
+  fetch(categoriesUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      categoryCreate(data.results);
     });
-  });
-  menuListContainer.style.display = "none"
+
+  fetch(itemsUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      itemCreate(data.results);
+      $grid = $(".menu-list-container").isotope({
+        itemSelector: ".menu-item",
+        layoutMode: "fitRows",
+      });
+    });
+  menuListContainer.style.display = "none";
 }
 function categoryCreate(categories) {
   for (let x in categories) {
@@ -35,11 +41,11 @@ function categoryCreate(categories) {
     var category_slug = categories[x].slug;
     var category_image = categories[x].image;
 
-    var swiper_slide = document.createElement("div")
-    swiper_slide.classList.add("swiper-slide")
+    var swiper_slide = document.createElement("div");
+    swiper_slide.classList.add("swiper-slide");
     swiper_slide.setAttribute("target-menu", `.c${category_id}`);
-    var categori_card = document.createElement("div")
-    categori_card.classList.add("categori-card")
+    var categori_card = document.createElement("div");
+    categori_card.classList.add("categori-card");
     swiperWrapper.append(swiper_slide);
     swiper_slide.append(categori_card);
     categori_card.innerHTML = `
@@ -49,45 +55,41 @@ function categoryCreate(categories) {
     <div class="categori-name">
       <span class="categori-name-span"> ${category_title} </span>
     </div>
-    `
+    `;
 
     swiper_slide.addEventListener("click", (event) => {
-      firstMenuListContainer.style.display = "none"
-      menuListContainer.style.display = "flex"
+      firstMenuListContainer.style.display = "none";
+      menuListContainer.style.display = "flex";
       var Target = event.target;
-      var targetClass = event.target.classList[0]
+      var targetClass = event.target.classList[0];
       var targetDom;
       if (targetClass == "categori-card") {
-        targetDom = event.target.parentElement
-      }
-      else if (targetClass == "categori-icon") {
+        targetDom = event.target.parentElement;
+      } else if (targetClass == "categori-icon") {
         targetDom = event.target.parentElement.parentElement;
-      }
-      else if (targetClass == "categori-icon-img") {
+      } else if (targetClass == "categori-icon-img") {
         targetDom = event.target.parentElement.parentElement.parentElement;
-      }
-      else if (targetClass == "categori-name") {
+      } else if (targetClass == "categori-name") {
         targetDom = event.target.parentElement.parentElement;
-      }
-      else if (targetClass == "categori-name-span") {
+      } else if (targetClass == "categori-name-span") {
         targetDom = event.target.parentElement.parentElement.parentElement;
-      }
-      else {
-        console.log("are you sure you click on write point?")
+      } else {
+        console.log("are you sure you click on write point?");
       }
       // console.log(targetDom.getAttribute("target-menu"))
       var target_menu = targetDom.getAttribute("target-menu");
-      filtering(target_menu)
-      console.log()
-    })
+      filtering(target_menu);
+      console.log();
+    });
   }
 }
 
 function itemCreate(items) {
   fetch(`${baseUrl}app/items/?title=&category=1&category__title__icontains=`)
-    .then(res => res.json()).then((data) => {
-      firstItem(data.results)
-    })
+    .then((res) => res.json())
+    .then((data) => {
+      firstItem(data.results);
+    });
   for (let x in items) {
     var item_id = items[x].id,
       item_title = items[x].title,
@@ -97,8 +99,8 @@ function itemCreate(items) {
       item_category = items[x].category,
       item_description = items[x].description,
       item_price = items[x].unit_price;
-    var menu_item = document.createElement("div")
-    menu_item.className = `menu-item c${item_category} row col-12 col-md-2`
+    var menu_item = document.createElement("div");
+    menu_item.className = `menu-item c${item_category} row col-12 col-md-2`;
     menu_item.innerHTML = `
       <div class="img-name">
        <div class="item-img">
@@ -123,7 +125,7 @@ function itemCreate(items) {
           </div>
         </div>
     `;
-    menuListContainer.append(menu_item)
+    menuListContainer.append(menu_item);
   }
 }
 
@@ -133,12 +135,11 @@ function filtering(value) {
   console.log(value);
 
   if (!$grid) {
-    $grid = $('.menu-list-container').isotope({
-      itemSelector: '.menu-item',
-      layoutMode: 'fitRows'
+    $grid = $(".menu-list-container").isotope({
+      itemSelector: ".menu-item",
+      layoutMode: "fitRows",
     });
-  }
-  else {
+  } else {
     $grid.isotope({ filter: value });
   }
 }
@@ -153,8 +154,8 @@ function firstItem(firstItem) {
       item_category = firstItem[x].category,
       item_description = firstItem[x].description,
       item_price = firstItem[x].unit_price;
-    var menu_item = document.createElement("div")
-    menu_item.className = `menu-item c${item_category} row col-12 col-md-2`
+    var menu_item = document.createElement("div");
+    menu_item.className = `menu-item c${item_category} row col-12 col-md-2`;
     menu_item.innerHTML = `
       <div class="img-name">
        <div class="item-img">
@@ -179,6 +180,6 @@ function firstItem(firstItem) {
           </div>
         </div>
     `;
-    firstMenuListContainer.append(menu_item)
+    firstMenuListContainer.append(menu_item);
   }
 }
